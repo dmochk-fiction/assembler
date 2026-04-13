7d1e1be148d5591786f8b60520f576d2e52a5772
Dima <Too sensetive data>
2026-04-13 12:08:28 +0300

    Almost done

--- lab4/prog.asm
+++ lab4/prog.asm
@@ -4,12 +4,17 @@ default rel
 extern printf, scanf, fgets, stdin, stdout, fflush
 extern fopen
 extern fclose
+extern fprintf
+extern asinh
 
 section .rodata
 
+two: dq 2.0
+minus: dq -1.0
 one: dq 1.0
+six: dq 6.0
 
-sign_mask: dq 0x7FFFFFFFFFFFFFFF ; 1 bit = 1, other 63 equals 0
+sign_mask: dq 0x7FFFFFFFFFFFFFFF ; 1 bit = 0, other 63 equals 1
 
 ; define messages 
 
@@ -36,6 +41,8 @@ input_dev: db "Enter deviation value: ", 0
 ; output formats for printf
 fmt_str: db "%s", 0 ; just for simple line of text
 fmt_dbl: db "%lf", 0 ; lonf float = double
+fmt_to_file: db "Number: %d", 9, "Value: %.40lf", 10, 0
+fmt_end: db "Left: %.40lf", 10, "Right: %.40lf", 10, 0
 
 mode_w: db "w+", 0
 
@@ -46,6 +53,9 @@ file_handler: resq 1; = FILE*
 filename: resq 1 ;
 
 ; 
+sum: resq 1; accumulate sum of numerical series there
+cur_a_n: resq 1; to avoid spoiling data after calls of libc functions
+num_iter: resq 1; reserve 64 bits to store number of current numerical series
 deval: resq 1 ; reserve 64 bits for double deviation value
 xval: resq 1 ; reserve 64 bits for double x value
 ret_code: resq 1 ; reserve memory to save return address
@@ -117,20 +127,89 @@ main:
 	; start with x + a_1
 	
 	movsd xmm0, qword [xval]
+	movsd xmm1, qword [xval]
+	mulsd xmm0, xmm1	
+	mulsd xmm0, xmm1 ; xmm0 = x^3
+	movsd xmm1, qword [minus]
+	mulsd xmm0, xmm1 ; xmm0 = -(x^3)
+	movsd xmm1, qword [six]
+	divsd xmm0, xmm1 ; xmm0 = (-1) * x^3 * (1/6)	
 
-
-
-
-
+	movsd qword [cur_a_n], xmm0
+	mov qword [sum], 0	
+	mov qword [num_iter], 1
+	; start cycle
+.loop:
+	mov rdi, qword [file_handler] ; rdi = FILE*
+	lea rsi, [fmt_to_file]
+	mov rdx, qword [num_iter] 
+	; xmm0 already has a_n value
+	mov rax, 1 ; rax = quantity of xmm-registers with data
+	call fprintf
 	
+	mov rdx, qword [num_iter]
+	inc rdx
+	mov qword [num_iter], rdx ;
+
+	; restore values
+	movsd xmm0, qword [cur_a_n]
+	movsd xmm1, qword [sum]
+	movsd xmm2, qword [deval] ; xmm2 stores deviation
+	addsd xmm1, xmm0
+	movsd qword [sum], xmm1 ; sum += cur_a_n
+
+	movsd xmm3, xmm0
+	movsd xmm4, qword [sign_mask] ;
+	andpd xmm3, xmm4; xmm3 = abs(xmm3)
+
+	ucomisd xmm2, xmm3 ; if xmm2(deviation) >= xmm3(current a_n)
+	jae epilog
+	
+
+
+	call quotient ; after call we have xmm3 = quotient
+	mulsd xmm0, xmm3 ; xmm0 = a_(n + 1)
+	movsd qword [cur_a_n], xmm0
+
+	jmp .loop
+
+epilog:
+	addsd xmm1, qword [xval] ; numerical series + x
+	movsd xmm0, qword [xval]
+	call asinh
+	
+	lea rdi, [rel fmt_end]
+	mov rax, 2
+	call printf
+
 prog_end:
-	
+
+
 	lea rdi, [rel fmt_str]
 	lea rsi, [rel prog_comp]
 	xor rax, rax
 	call printf
 
 	add rsp, 8
+	ret
+
+
+quotient:
+	movsd xmm3, qword [xval] ; xmm3 = x
+	mulsd xmm3, xmm3 ; xmm3 = x^2
+	movsd xmm4, qword[minus] ; xmm4 = -1.0
+	mulsd xmm3, xmm4 ; xmm3 = -x^2
+	movsd xmm4, qword [num_iter]; xmm4 = n
+	mulsd xmm4, qword [two] ; xmm4 = 2*n
+	addsd xmm4, qword [one] ; xmm4 = 2*n + 1
+	movsd xmm5, xmm4 ; xmm5 = xmm4 = 2*n +1
+	mulsd xmm4, xmm4 ; xmm4 = (2*n + 1)^2
+	mulsd xmm3, xmm4 ; xmm3 = -x^2 * (2n + 1)^2
+	addsd xmm5, qword [one] ; xmm5 = (2n + 2)
+	movsd xmm4, xmm5 ; xmm4 = (2n + 2)
+	addsd xmm4, qword [one] ; xmm4 = (2n + 3)
+	mulsd xmm4, xmm5 ; xmm4 = (2n + 2) * (2n + 3)
+	divsd xmm3, xmm4 ; xmm3 = -x^2 * (2n + 1)^2 / ((2n + 2)(2n + 3))
 	ret
 
 few_arg:
